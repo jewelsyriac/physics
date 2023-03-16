bb66f10b64ee15e8f95d296cd8778b4932059b7d
Jewel Syriac <114583922+jewelsyriac@users.noreply.github.com>
2023-03-16 23:45:51 +0530

    Add files via upload

--- pht110.js
+++ pht110.js
@@ -110,6 +110,70 @@ closeDialogButtons.forEach(closeDialogButton => {
 });
 
 
+const generatorButton = document.getElementById("questionGeneratorButton");
+generatorButton.addEventListener('click', event => {
+    var inputField = document.getElementById("input-field");
+    var number = parseInt(inputField.value);
+    var divisionTime = Math.floor(number / 10);
+    var questionSpace = document.getElementById("questions");
+    var questionList = questionSpace.querySelector("ol");
+    var newQuestion1 = document.createElement("li");
+    newQuestion1.textContent = "A damped harmonic oscillator has a Q-value of "+(number*250)+" and a frequency of "+(number*10)+" Hz. Find the time required for the energy of the oscillator to decrease to 1/"+(divisionTime +2)+" of its initial value. ";
+    questionList.appendChild(newQuestion1);
+    var newQuestion2 = document.createElement("li");
+    newQuestion2.textContent = "A damped oscillator of mass "+(divisionTime +10)+"kg has a force constant "+ (number+50)+" N/m and damping constant "+(divisionTime + 1)+" s-1. Find the angular frequency with this damping.";
+    questionList.appendChild(newQuestion2);
+    questionSpace.classList.add('active');
+    var answerSpace = document.getElementById("answers");
+    var answerList = answerSpace.querySelector("ol");
+    var answer1 = ((Math.log(divisionTime +2))/(2*0.125)).toFixed(2);
+    var naturalFrequency = (number+50)/(divisionTime +10)
+    var bsquare = (divisionTime + 1.0)**2
+    var fourM = 4*(divisionTime +10)
+    var answer2 = Math.sqrt(naturalFrequency - (bsquare/fourM));
+    var newAnswer = document.createElement("li");
+    newAnswer.textContent = answer1+" seconds.";
+    var newAnswer1 = document.createElement("li");
+    newAnswer1.textContent = answer2+" Hz.";
+    const answerButton = document.getElementById("answerbutton");
+    const answerField = document.getElementById('answer-input1');
+  const submitButton = document.getElementById('submit-button1');
+  const resultElement = document.getElementById('result');
+  submitButton.addEventListener('click', () => {
+    const inputValue = answerField.value;
+    if (inputValue === answer1) {
+      resultElement.textContent = 'Great, Correct answer!';
+      document.body.style.backgroundColor = "rgb(230, 255, 230)";
+    } else {
+      resultElement.textContent = 'Incorrect. Please try again.';
+      document.body.style.backgroundColor = "rgb(255, 230, 230)";
+    }
+  });
+    const answerField2 = document.getElementById('answer-input2');
+  const submitButton2 = document.getElementById('submit-button2');
+  const resultElement2 = document.getElementById('result2');
+  submitButton2.addEventListener('click', () => {
+    const inputValue2 = answerField2.value;
+    if (inputValue2 === answer2) {
+      resultElement2.textContent = 'Great, Correct answer!';
+      document.body.style.backgroundColor = "rgb(230, 255, 230)";
+    } else {
+      resultElement2.textContent = 'Incorrect. Please try again.';
+      document.body.style.backgroundColor = "rgb(255, 230, 230)";
+    }
+  });
+answerButton.addEventListener('click', event => {
+    answerList.appendChild(newAnswer);
+    answerList.appendChild(newAnswer1);
+    answerSpace.style.display="block";
+    
+    
+});
+});
+
+
+  
+ 
 
   
 
